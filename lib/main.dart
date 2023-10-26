46c0acb9c58b3a2379e5b2d632f79f097b56d47b
Emile005 <izzatilloabdumannonov11@gmail.com>
2023-10-26 19:29:45 +0500

    my first

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:first_project/screens/home_ekran.dart';
+import 'package:flutter/material.dart';
+
+// import 'screens/home_screen.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+
+
+
+
+
+
